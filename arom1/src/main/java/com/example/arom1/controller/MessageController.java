8d0c2a5a14453c9378bee62a18440e3e521a5b8c
arom1_entity <sn15dueo@naver.com>
2024-08-01 16:00:38 +0900

    WebSocketConfig추가

---
+++ arom1/src/main/java/com/example/arom1/controller/MessageController.java
@@ -0,0 +1,19 @@
+package com.example.arom1.controller;
+
+import com.example.arom1.dto.MessageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MessageController {
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
+
+    @MessageMapping("/hello")
+    public void message(MessageDto messageDto){
+        simpMessageSendingOperations.convertAndSend("/sub/channel/" + messageDto.getChannelId(), messageDto);
+    }
+}
---
+++ arom1/src/main/java/com/example/arom1/dto/MessageDto.java
@@ -0,0 +1,29 @@
+package com.example.arom1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDto {
+    private String type;
+    private String sender;
+    private String channelId;
+    private String data;
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void newConnect(){
+        this.type="new";
+    }
+
+    public void closeConnect(){
+        this.type="close";
+    }
+}
