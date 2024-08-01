f3303b358a3338ba17a71f490b895fa9e6c9d926
discipline24 <ejsky33@naver.com>
2024-08-01 13:52:24 +0900

    fix: 중복 코드 삭제

--- arom1/src/main/java/com/example/arom1/common/response/BaseResponseStatus.java
+++ arom1/src/main/java/com/example/arom1/common/response/BaseResponseStatus.java
@@ -14,11 +14,12 @@ public enum BaseResponseStatus {
 
     // 4xx : client error
     EXIST_EMAIL(false, HttpStatus.CONFLICT.value(), "이미 존재하는 회원입니다."),
-    FAIL_LOGIN(false, HttpStatus.NO_CONTENT.value(), "로그인에 실패했습니다."),
     EXIST_NICKNAME(false, HttpStatus.CONFLICT.value(), "이미 존재하는 닉네임입니다."),
+
     NON_EXIST_USER(false, HttpStatus.NO_CONTENT.value(), "존재하지 않는 회원입니다."),
     PASSWORD_ERROR(false, HttpStatus.NO_CONTENT.value(), "비밀번호가 틀렸습니다."),
     HTTP_METHOD_ERROR(false, HttpStatus.FORBIDDEN.value(), "http 메서드가 올바르지 않습니다."),
+
     NO_EATERY_BY_KEYWORD(false, HttpStatus.NO_CONTENT.value(), "검색어로 된 음식점이 없습니다."),
     NO_EATERY_BY_ADDRESS(false, HttpStatus.NO_CONTENT.value(), "주변에 음식점이 없습니다."),
 
