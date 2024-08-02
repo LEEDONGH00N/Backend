9f86c7177928222bbcd266e9fac821fbf782e500
discipline24 <ejsky33@naver.com>
2024-08-02 23:10:54 +0900

    fix: bindingResult 수정중

--- arom1/src/main/java/com/example/arom1/controller/MemberController.java
+++ arom1/src/main/java/com/example/arom1/controller/MemberController.java
@@ -2,15 +2,15 @@ package com.example.arom1.controller;
 
 import com.example.arom1.common.exception.BaseException;
 import com.example.arom1.common.response.BaseResponse;
-import com.example.arom1.dto.MemberDto;
-import com.example.arom1.entity.Member;
+import com.example.arom1.dto.request.SignupRequest;
+import com.example.arom1.dto.response.SignupResponse;
 import com.example.arom1.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,25 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
-
     private final MemberService memberService;
-    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/signup")
-    public BaseResponse<MemberDto> signup(@RequestBody MemberDto request) {
+    public BaseResponse<SignupResponse> signup(@RequestBody SignupRequest request,
+                                                 BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            bindingResult.getFieldError()
+        }
+
         try {
-            Member member = Member.createMember(request, passwordEncoder);
-            memberService.saveMember(member);
+            return new BaseResponse<>(SignupResponse
+                    .MemberToSignupResponse(memberService.saveMember(request)));
         } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus());
         }
-        return new BaseResponse<>(request);
     }
-//    @GetMapping("/logout")
-//    public String logout(HttpServletRequest request, HttpServletResponse response) {
-//        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
-//
-//        return "redirect:/login";
-//    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+
+        return "redirect:/login";
+    }
 
 }
