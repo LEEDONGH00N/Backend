71332b51d4cf20a5f1862fc58cff691c8e38b50a
discipline24 <ejsky33@naver.com>
2024-08-03 23:53:57 +0900

    feat: bindingResult로 dto 예외 처리

--- arom1/src/main/java/com/example/arom1/controller/MemberController.java
+++ arom1/src/main/java/com/example/arom1/controller/MemberController.java
@@ -4,12 +4,13 @@ import com.example.arom1.common.exception.BaseException;
 import com.example.arom1.common.response.BaseResponse;
 import com.example.arom1.dto.request.LoginRequest;
 import com.example.arom1.dto.request.SignupRequest;
-import com.example.arom1.dto.response.LoginResponse;
 import com.example.arom1.dto.response.SignupResponse;
 import com.example.arom1.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.validation.BindingResult;
@@ -27,31 +28,37 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public BaseResponse<SignupResponse> signup(@RequestBody SignupRequest request,
+    public BaseResponse<SignupResponse> signup(@Valid @RequestBody SignupRequest request,
                                                  BindingResult bindingResult) {
+        //에러 있으면 로그 출력 후 에러메시지 출력
         if(bindingResult.hasErrors()) {
-            List<String> errors = new ArrayList<>();
-            bindingResult.getAllErrors().forEach(error ->  errors.add(error.getDefaultMessage()));
-            return new BaseResponse<>(SignupResponse.ErrorMessagesToSignupResponse(errors));
+            return new BaseResponse<>(false, HttpStatus.BAD_REQUEST.value(), findErrorMessage(bindingResult));
         }
 
         try {
             return new BaseResponse<>(SignupResponse
                     .MemberToSignupResponse(memberService.saveMember(request)));
+
         } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus());
         }
     }
     @PostMapping("/login")
-    public BaseResponse<String> login() {
+    public BaseResponse<String> login(@Valid @RequestBody LoginRequest loginRequest,
+                                      BindingResult bindingResult, HttpServletResponse response) {
+        if(bindingResult.hasErrors()) {
+            return new BaseResponse<>(false, HttpStatus.BAD_REQUEST.value(), findErrorMessage(bindingResult));
+        }
+
         try {
-            System.out.println("Hello");
+            String jwtToken =  memberService.login(loginRequest);
+            response.addHeader("Authorization", "Bearer " + jwtToken);
+            return new BaseResponse<>("Login Success");
         }
         catch(BaseException e) {
             return new BaseResponse<>(e.getStatus());
         }
-        return new BaseResponse<>("Success");
-        //return "redirect:/login";
+
     }
 
     @GetMapping("/logout")
@@ -59,6 +66,14 @@ public class MemberController {
         new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
 
         return "redirect:/login";
+    }
+
+    private String findErrorMessage(BindingResult bindingResult) {
+        List<String> messages = new ArrayList<>();
+        bindingResult.getAllErrors().forEach(error ->  messages.add(error.getDefaultMessage()));
+        for(String msg : messages) System.out.println(msg);
+
+        return messages.get(0);
     }
 
 }
