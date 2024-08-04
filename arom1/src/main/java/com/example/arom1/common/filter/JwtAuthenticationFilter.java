fde2196194fe42b6efef826640add0e9b3976690
discipline24 <ejsky33@naver.com>
2024-08-05 00:39:12 +0900

    fix: 인증 필터 이름 변경 - JwtAuthenticationFilter

--- arom1/src/main/java/com/example/arom1/common/filter/JwtAuthenticationFilter.java
+++ arom1/src/main/java/com/example/arom1/common/filter/JwtAuthenticationFilter.java
@@ -3,41 +3,36 @@ package com.example.arom1.common.filter;
 import com.example.arom1.common.response.BaseResponse;
 import com.example.arom1.common.response.BaseResponseStatus;
 import com.example.arom1.common.util.jwt.TokenProvider;
-import com.example.arom1.entity.security.MemberDetail;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class JwtAuthorizationFilter extends OncePerRequestFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final TokenProvider tokenProvider;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public JwtAuthorizationFilter(TokenProvider tokenProvider) {
+    public JwtAuthenticationFilter(TokenProvider tokenProvider) {
         this.tokenProvider = tokenProvider;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //로그인, 회원가입 시 인가 필터 무시
+        //로그인, 회원가입 시 인증 필터 무시
         String requestURI = request.getRequestURI();
         if (requestURI.equals("/login") || requestURI.equals("/signup")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        System.out.println("JwtAuthorizationFilter 인가 : 진입");
+        System.out.println("JwtAuthenticationFilter 인증 : 진입");
 
         String token = tokenProvider.resolveToken(request);
 
