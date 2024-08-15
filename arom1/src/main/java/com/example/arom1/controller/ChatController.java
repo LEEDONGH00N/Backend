ed0c4a07599b20dcf54ae00f1904073881716620
DaeunSon <sn15dueo@naver.com>
2024-08-15 13:17:46 +0900

    ENTER / LEAVE 로직 추가

--- arom1/src/main/java/com/example/arom1/controller/ChatController.java
+++ arom1/src/main/java/com/example/arom1/controller/ChatController.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
@@ -17,13 +15,29 @@ public class ChatController {
     private final SimpMessageSendingOperations simpMessageSendingOperations;
     private final TokenProvider tokenProvider;
 
-    @MessageMapping("/chat/message") //websocket "pub/chat/message"로 들어오는 메세지 처리
-    public void sendMessage(ChatMessage chatMessage, @Header("Authorization")String Authorization ) {
+    @MessageMapping("/chat/message/{chatroomId}") //websocket "pub/chat/message"로 들어오는 메세지 처리
+    public void sendMessage(@DestinationVariable Long chatRoomId, ChatMessage chatMessage, @Header("Authorization")String Authorization ) {
         String authorization = tokenProvider.extractJwt(Authorization);
-        Object memberId = tokenProvider.getClaims(authorization).get("id");
-       chatMessage.setSender((String) memberId);
+        Object memberId = tokenProvider.getClaims(authorization).get("memberId");
+        chatMessage.setSender((String) memberId);
+
+        if(isENTER(chatMessage)){
+            chatMessage.setMessage(chatMessage.getSender() + "님이 입장하셨습니다.");
+        }
+
+        if(isLEAVE(chatMessage)){
+            chatMessage.setMessage(chatMessage.getSender() + "님이 퇴장하셨습니다.");
+        }
 
         // 특정 채팅방에 메시지를 브로드캐스트
         simpMessageSendingOperations.convertAndSend("/sub/chat/room" + chatMessage.getChatroomId(), chatMessage);
+    }
+
+    private boolean isENTER(ChatMessage messageType) {
+        return messageType.getMessageType().equals(ChatMessage.MessageType.ENTER);
+    }
+
+    private boolean isLEAVE(ChatMessage messageType){
+        return messageType.getMessageType().equals(ChatMessage.MessageType.LEAVE);
     }
 }
